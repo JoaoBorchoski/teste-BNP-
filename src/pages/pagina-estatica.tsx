0e70bbe4ceb7aee7e5b9cd7497535de087242b35
JoaoBorchoski <joaoborchoskidev@gmail.com>
2024-06-05 23:21:48 -0300

    Feat: Finalizado item de regenerar a pagina estatica

--- src/pages/pagina-estatica.tsx
+++ src/pages/pagina-estatica.tsx
@@ -7,21 +7,40 @@
  * - A página deve ser atualizada a cada 1 minuto
  */
 
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 import styles from "@/styles/lista.module.css";
 import { ICity } from "@/types/city.d";
 import { GetStaticProps } from "next";
-import { generateCities } from "./api/cities/[length]";
 
 export default function Lista({ initialData }: { initialData: Array<ICity> }) {
+  const [cities, setCities] = useState<ICity[]>(initialData);
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/cities/10`,
+          { method: "GET" }
+        );
+        const data = await response.json();
+        if (!response.ok) throw new Error("Erro ao obter os dados");
+
+        setCities(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }, 60000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <h2>Lista de cidades</h2>
 
         <div data-list-container>
-          {initialData.map((city) => (
+          {cities.map((city) => (
             <div data-list-item key={city.id}>
               {city.name}
             </div>
@@ -41,17 +60,13 @@ export const getStaticProps: GetStaticProps = async () => {
     const initialData = await response.json();
     if (!response.ok) throw new Error("Erro ao obter os dados");
 
-    // const initialData = generateCities(10);
-
     return {
       props: { initialData },
-      revalidate: 10,
     };
   } catch (error) {
     console.error(error);
     return {
       props: { initialData: [] },
-      revalidate: 10,
     };
   }
 };
